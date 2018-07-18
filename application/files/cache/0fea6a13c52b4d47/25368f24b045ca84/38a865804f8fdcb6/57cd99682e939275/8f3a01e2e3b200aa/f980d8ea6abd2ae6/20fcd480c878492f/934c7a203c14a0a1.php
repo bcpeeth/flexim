2ecaf80e48f9f0adf5177169a06162306b2fe1f7
Pedro Maria <34841898+bcpeeth@users.noreply.github.com>
2018-07-18 21:09:38 +0200

    adds more styling

--- application/files/cache/0fea6a13c52b4d47/25368f24b045ca84/38a865804f8fdcb6/57cd99682e939275/8f3a01e2e3b200aa/f980d8ea6abd2ae6/20fcd480c878492f/934c7a203c14a0a1.php
+++ application/files/cache/0fea6a13c52b4d47/25368f24b045ca84/38a865804f8fdcb6/57cd99682e939275/8f3a01e2e3b200aa/f980d8ea6abd2ae6/20fcd480c878492f/934c7a203c14a0a1.php
@@ -1,12 +1,12 @@
 <?php 
 /* Cachekey: cache/stash_default/zend/zend_i18n_translator_messages_a140ff919366c14208fefb25ebc9b1cd/ */
 /* Type: array */
-/* Expiration: 2018-07-23T15:01:25+02:00 */
+/* Expiration: 2018-07-23T06:22:01+02:00 */
 
 
 
 $loaded = true;
-$expiration = 1532350885;
+$expiration = 1532319721;
 
 $data = array();
 
@@ -14,4 +14,4 @@ $data = array();
 $data['return'] = unserialize(base64_decode('Tjs='));
 
 /* Child Type: integer */
-$data['createdOn'] = 1531925772;
+$data['createdOn'] = 1531940903;
--- packages/flexim/themes/flexim/default.php
+++ packages/flexim/themes/flexim/default.php
@@ -1,11 +1,12 @@
 <?php Loader::packageElement('header', 'flexim'); ?>
 
 <section class="hero">
-
+  <img class="path" src="<?php echo $view->getThemePath(); ?>/assets/images/hero.jpg">
 </section>
 
-<section class="intro ">
-  <div class="grid__container padding--top-2-steps padding--bottom-2-steps">
+<section class="intro padding--top-3-steps padding--bottom-3-steps">
+  <div class="grid__container grid__visual">
+
     <div class="grid__col--6">
       <h3 class="padding--bottom-step">Oprichting</h3>
       <p>De Flexim Foundation is opgericht op … in Dordrecht op initiatief van een aantal medewerkers van Flexim Netherlands.
@@ -14,14 +15,40 @@
       De Flexim Foundation zet zich in voor diverse kwetsbare groepen binnen en buiten de Nederlandse samenleving. Deze kwetsbare groepen worden geholpen om hun levenscondities te verbeteren en toekomstperspectief te vergroten. 
       </p>
     </div>
-  </div>
-  <div class="grid__col--6">
+
+    <div class="grid__col--4 grid__push--2">
+      <img class="" src="<?php echo $view->getThemePath(); ?>/assets/images/Tekengebied-1.png">
+    </div>
 
   </div>
 </section>
 
-<section class="description bg-lightgrey">
-  <div class="grid__container padding--top-2-steps padding--bottom-2-steps">
+
+<section class="goal bg-red white clearfix">
+  <div class="grid__container padding--top-3-steps padding--bottom-3-steps text--align-center">
+    
+    <div class="grid__col--8 grid__push--2">
+      <h3 class="padding--bottom-step">Doelstelling</h3>
+      <p>“De Flexim Foundation stelt zich ten doel de levenscondities en toekomstmogelijkheden van kwetsbare groepen (in de Nederlandse samenleving en daarbuiten) te verbeteren. De Flexim Foundation tracht dit doel te bereiken door financiële ondersteuning van en actieve bijdrage aan de uitvoering van projecten conform het gestelde doel.”</p>
+    </div>
+  </div>
+</section>
+
+<section class="target">
+  <div class="grid__container padding--top-2-steps padding--bottom-2-steps grid__visual">
+    
+    <div class="grid__col--12 grid__collapse">
+    <div class="list-horizontal">
+      <ul>
+        <li>financiële middelen te werven via sponsoring en giften</li>
+        <li>continuïteit te waarborgen vanuit Flexim Netherlands</li>
+        <li>werkgevers te motiveren om tijd/arbeid en kennis beschikbaar te stellen door vrijwilligerswerk van hun werknemers</li>
+        <li>medewerkers en klanten van Flexim aan te sporen zich in te zetten bij de activiteiten van de foundation</li>
+        <li>particulieren alsmede instanties in te zetten voor het verwerven van de middelen alsmede beschikbaar stellen van arbeid</li>      
+      </ul>
+    </div>
+</div>
+   
   </div>
 
 </section>
