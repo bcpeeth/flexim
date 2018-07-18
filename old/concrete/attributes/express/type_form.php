a15567345bc2634c1a2180413388b7b2f91968d3
Pedro Maria <34841898+bcpeeth@users.noreply.github.com>
2018-07-18 16:33:03 +0200

    cleans git log from deleted files

--- old/concrete/attributes/express/type_form.php
+++
@@ -1,9 +0,0 @@
-<fieldset class="ccm-attribute ccm-attribute-date-time">
-<legend><?php echo t('Express Options')?></legend>
-
-<div class="form-group">
-<?php echo $form->label('exEntityID', t('Entity'))?>
-<?php echo $form->select('exEntityID', $entities, $entityID)?>
-</div>
-
-</fieldset>
--- old/concrete/elements/express/form/view/text.php
+++
@@ -1,8 +0,0 @@
-<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
-
-<?php if ($control->getHeadline()) { ?>
-    <h3><?php echo $control->getHeadline()?></h3>
-<?php } ?>
-<?php if ($control->getBody()) { ?>
-    <?php echo $control->getBody()?>
-<?php } ?>
