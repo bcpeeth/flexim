71263ee86ce624228b5ad41fa375257ec651535d
Pedro Maria <34841898+bcpeeth@users.noreply.github.com>
2018-07-18 16:56:40 +0200

    adds latest changes

--- packages/flexim/themes/flexim/default.php
+++ packages/flexim/themes/flexim/default.php
@@ -1,5 +1,29 @@
 <?php Loader::packageElement('header', 'flexim'); ?>
 
+<section class="hero">
 
+</section>
+
+<section class="intro ">
+  <div class="grid__container padding--top-2-steps padding--bottom-2-steps">
+    <div class="grid__col--6">
+      <h3 class="padding--bottom-step">Oprichting</h3>
+      <p>De Flexim Foundation is opgericht op … in Dordrecht op initiatief van een aantal medewerkers van Flexim Netherlands.
+      <br />
+      <br />
+      De Flexim Foundation zet zich in voor diverse kwetsbare groepen binnen en buiten de Nederlandse samenleving. Deze kwetsbare groepen worden geholpen om hun levenscondities te verbeteren en toekomstperspectief te vergroten. 
+      </p>
+    </div>
+  </div>
+  <div class="grid__col--6">
+
+  </div>
+</section>
+
+<section class="description bg-lightgrey">
+  <div class="grid__container padding--top-2-steps padding--bottom-2-steps">
+  </div>
+
+</section>
 
 <?php Loader::packageElement('footer', 'flexim'); ?>
