7b969707448b42955b2db8cdc98d9d96b70e2b9d
bcpeeth <34841898+bcpeeth@users.noreply.github.com>
2018-11-05 22:37:46 +0100

    adds flexible text block to 'news' page

--- packages/flexim/themes/flexim/nieuws.php
+++ packages/flexim/themes/flexim/nieuws.php
@@ -1,35 +1,36 @@
 <?php Loader::packageElement('header', 'flexim'); ?>
 
 <section class="hero">
-    <img class="top" src="<?php echo $view->getThemePath(); ?>/assets/images/boys.jpg">
+    <img class="top" src="<?php echo $view->getThemePath(); ?>/assets/images/teamwork.jpg">
 </section>
 
 <section class="nieuws">
     
     <div class="nieuws-intro bg-blue text--align-center white">
         <div class="grid__container padding--top-3-steps padding--bottom-3-steps">
+
             <div class="grid__col--10 grid__push--1">
                 <h3 class=" text--align-center padding--bottom-nano">Flexim Foundation is continu in ontwikkeling.</h3>
-                </div>
-                <div class="grid__col--8 grid__push--2">
-                <p>Daarbij zijn de medewerkers, klanten en relaties van Flexim intensief betrokken om samen een bijdrage te leveren aan aan diverse maatschappelijke doelen in binnen en buitenland. Volg hieronder onze laatste ontwikkelingen: </p>
-                </div>
             </div>
+            <div class="grid__col--8 grid__push--2">
+                <p>Daarbij zijn de medewerkers, klanten en relaties van Flexim intensief betrokken om samen een bijdrage te leveren aan aan diverse maatschappelijke doelen in binnen en buitenland. Volg hieronder onze laatste ontwikkelingen: </p>
+            </div>
+
         </div>
     </div>
 
-
-    <!-- <?php 
-        $a = new Area('Nieuws');
-        $a->display();
-    ?> -->
+    <!-- <div class="nieuws-title clearfix">
+        <div class="grid__col--12 padding--top-2-steps text--align-center">  
+            <h3 class="">Tour de Flexim brengt € 12.122,- op!</h3>
+        </div>
+    </div> -->
 
     <div class="nieuws-block">
-        <div class="grid__container  grid__visual padding--top-3-steps padding--bottom-3-steps ">
+        <div class="grid__container padding--top-2-steps">
 
-            <div class="grid__col--6 grid__col--sm-12">
-                <div class="nieuws-block__text nieuws-block__text--left">
-                    <h3 class="padding--bottom-step">Tour de Flexim brengt € 12.122,- op!</h3>
+            <div class="grid__col--6 grid__col--sm-12 bg-yellow">
+                <div class="nieuws-block-text nieuws-block-text--left">
+                    <h4 class="padding--bottom-half-step">Tour de Flexim brengt € 12.122,- op!</h4>
                     <p>Wat een geweldige prestatie! Na een tocht van 955 kilometer kwamen zaterdag 1 september om 16.00 uur de fietsers en het support team aan in Hoek van Holland. Hiermee hebben zij €12.122,- ingezameld voor twee projecten. 
                     <br />
                     <br />
@@ -39,7 +40,57 @@
             </div>
 
             <div class="grid__col--6 grid__col--sm-12">
-                <div class="nieuws-block__image nieuws-block__image--right">
+                <div class="nieuws-block-image nieuws-block__image--right">
+                    <img class="" src="<?php echo $view->getThemePath(); ?>/assets/images/tourdeflexim.jpg">
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="nieuws-block bg-lightgrey">
+        <div class="grid__container padding--top-2-steps padding--bottom-2-steps">
+
+            <div class="grid__col--6 grid__col--sm-12">
+                <div class="nieuws-block-image nieuws-block__image--right">
+                    <img class="" src="<?php echo $view->getThemePath(); ?>/assets/images/tourdeflexim.jpg">
+                </div>
+            </div>
+
+            <div class="grid__col--6 grid__col--sm-12 bg-yellow">
+                <div class="nieuws-block-text nieuws-block-text--right">
+                    <h4 class="padding--bottom-half-step">Tour de Flexim brengt € 12.122,- op!</h4>
+                    <p>Het team bestaat uit 10 wielrenners en 8 begeleiders. Er wordt gefietst in duo’s, dat wil zeggen dat er per duo ongeveer 180 km wordt afgelegd in etappes. Daarnaast bestaat de begeleiding uit chauffeurs, verzorgers, cateraars, een masseuse en mensen voor de fotografie.</p>
+                    <br />
+                    <p>Met dit kick-off evenement van de Flexim Foundation wordt geld ingezameld voor twee projecten:
+                    <br />
+                    1. Het organiseren van een kerstdiner voor 10 gezinnen in Nederland, die onder de armoedegrens leven.
+                    <br />
+                    2. Het inrichten van een kindertehuis in Oekraïne; dit in samenwerking met de Stichting Breath.</p>
+                    <br />
+                    <p> De tour is gestart op donderdag 30 augustus a.s. om 16.00 uur vanaf het hoofdkantoor van Flexim in Lausanne. Vanuit Zwitserland fietst de groep via het noordoosten van Frankrijk, dwars door België en West-Brabant (o.a. door Breda) om uiteindelijk zaterdagmiddag om 16.00 uur te finishen in Hoek van Holland. Wilt u de fietsers live volgen? Neem dan contact op via de website.</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="nieuws-block">
+        <div class="grid__container padding--top-2-steps">
+
+            <div class="grid__col--6 grid__col--sm-12 bg-yellow">
+                <div class="nieuws-block-text nieuws-block-text--left">
+                    <h4 class="padding--bottom-half-step">Tour de Flexim brengt € 12.122,- op!</h4>
+                    <p>Wat een geweldige prestatie! Na een tocht van 955 kilometer kwamen zaterdag 1 september om 16.00 uur de fietsers en het support team aan in Hoek van Holland. Hiermee hebben zij €12.122,- ingezameld voor twee projecten. 
+                    <br />
+                    <br />
+                    Wij willen iedereen bedanken die een bijdrage heeft geleverd aan dit evenement en in het bijzonder alle donateurs!
+                    </p>
+                </div>
+            </div>
+
+            <div class="grid__col--6 grid__col--sm-12">
+                <div class="nieuws-block-image nieuws-block__image--right">
                     <img class="" src="<?php echo $view->getThemePath(); ?>/assets/images/tourdeflexim.jpg">
                 </div>
             </div>
